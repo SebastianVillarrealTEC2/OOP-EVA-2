436197a9164b75134f52f2913fc4187c1039515b
SebastianVillarrealTEC2 <L25550588@chihuahua2.tecnm.mx>
2026-03-11 22:47:16 -0600

    Add files via upload

---
+++ EVA2_1_HERENCIA/src/main/java/com/mycompany/eva2_1_herencia/EVA2_1_HERENCIA.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.eva2_1_herencia;
+
+/**
+ *
+ * @author villa
+ */
+public class EVA2_1_HERENCIA {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        Ejemplo ejemplo= new Ejemplo();
+        System.out.println(ejemplo.toString());
+    }
+}
+    
+    
+class Ejemplo{
+    
+}
+
+
+
