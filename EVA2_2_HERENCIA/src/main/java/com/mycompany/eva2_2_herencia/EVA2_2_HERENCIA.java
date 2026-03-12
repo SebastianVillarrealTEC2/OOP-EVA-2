ea4fec8db4c6a26abfdd0d1d7bb992bf6cd056e4
SebastianVillarrealTEC2 <L25550588@chihuahua2.tecnm.mx>
2026-03-11 22:47:39 -0600

    Add files via upload

---
+++ EVA2_2_HERENCIA/src/main/java/com/mycompany/eva2_2_herencia/EVA2_2_HERENCIA.java
@@ -0,0 +1,69 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.eva2_2_herencia;
+
+/**
+ *
+ * @author villa
+ */
+public class EVA2_2_HERENCIA {
+
+    public static void main(String[] args) {
+        Empleado empleado = new Empleado();
+        //empleado.
+    }
+}
+class Persona {
+    private String nombre;
+    private String apellido;
+    private int edad;
+
+    public Persona() {
+        this.nombre = "";
+        this.apellido = "";
+        this.edad = 0;
+    }
+
+    public Persona(String nombre, String apellido, int edad) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.edad = edad;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+}
+    
+class Empleado extends Persona {
+
+}
+
+class Cliente extends Persona {
+}
+
+class Proveedor extends Persona {
+}
+
