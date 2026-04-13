d67ecd47b22221d8b8edc11625c28eefd821f186
SebastianVillarrealTEC2 <L25550588@chihuahua2.tecnm.mx>
2026-04-12 21:03:49 -0600

    Add files via upload

---
+++ EVA2_8_CLASESABSTRACT/src/main/java/com/mycompany/eva2_8_clasesabstract/EVA2_8_CLASESABSTRACT.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.eva2_8_clasesabstract;
+
+/**
+ *
+ * @author bisonte
+ */
+public class EVA2_8_CLASESABSTRACT {
+
+    public static void main(String[] args) {
+        Persona persona = new Persona("sebastian", "villarreal", 19);
+        System.out.println(persona);
+    }
+}
---
+++ EVA2_8_CLASESABSTRACT/src/main/java/com/mycompany/eva2_8_clasesabstract/Persona.java
@@ -0,0 +1,56 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.eva2_8_clasesabstract;
+
+/**
+ *
+ * @author bisonte
+ */
+public class Persona {
+    protected String nombre;
+    protected String apellidos; 
+    protected int edad;
+    
+    public Persona() {
+        this.nombre = "";
+        this.apellidos = "";
+        this.edad =0;
+    }
+
+    public Persona(String nombre, String apellidos, int edad) {
+        this.nombre = nombre;
+        this.apellidos = apellidos;
+        this.edad = edad;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    @Override
+    public String toString() {
+        return "Persona{" + "nombre=" + nombre + ", apellidos=" + apellidos + ", edad=" + edad + '}';
+    }
+}
