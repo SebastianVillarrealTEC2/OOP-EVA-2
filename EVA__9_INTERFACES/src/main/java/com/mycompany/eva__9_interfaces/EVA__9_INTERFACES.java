951a5d835318935209e34102ede6beec66a2fa67
SebastianVillarrealTEC2 <L25550588@chihuahua2.tecnm.mx>
2026-04-12 21:12:46 -0600

    Add files via upload

---
+++ EVA__9_INTERFACES/src/main/java/com/mycompany/eva__9_interfaces/EVA__9_INTERFACES.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.eva__9_interfaces;
+
+/**
+ *
+ * @author bisonte
+ */
+public class EVA__9_INTERFACES {
+
+    public static void main(String[] args) {
+        Persona persona = new Persona("juan", "perez", 50);
+        //Mensaje mensaje = new Mensaje no instacion
+        persona.mostrarMensaje();
+    }
+}
